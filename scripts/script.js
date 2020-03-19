3fe16ecd8813a26185a3b0f4f247c419bfed670c
Bounty556 <jacobmayday@gmail.com>
2020-03-18 22:12:46 -0600

    Started JS

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+document.getElementById('generate-password').addEventListener('click', function() {
+    // Generate password based on clicked options
+    
+    document.getElementById('generator-text').textContent = "Button pressed";
+});
